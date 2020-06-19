cb6321014494388dc8296152d2c5843932ca2712
Viktor Demydov <vicd129@gmail.com>
2020-06-19 15:10:32 +0300

    project structure update

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// Collapse
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "flex") {
+        content.style.display = "none";
+        } else {
+            content.style.display = "flex";
+        }
+    });
+}
+
+
+// ScrollTrigger animations
+let tl1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".details_wrapper",
+        start: "top center"
+    }
+});
+
+tl1.from(".details_h1", {y: 300, opacity: 0, duration: 1})
+   .from(".detail1_h2", {y: 300, opacity: 0, duration: 1})
+   .from(".detail1_img", {y: 300, opacity: 0, duration: 1});
+
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".detail2",
+        start: "top center"
+    }
+});
+
+tl2.from(".detail2_h2", {y: 300, opacity: 0, duration: 1})
+   .from(".detail2_img", {y: 300, opacity: 0, duration: 1});
+
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".detail3",
+        start: "top center"
+    }
+});
+
+tl3.from(".detail3_h2", {y: 300, opacity: 0, duration: 1})
+   .from(".detail3_img", {y: 300, opacity: 0, duration: 1});
+
+let tl4 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".detail4",
+        start: "top center"
+    }
+});
+
+tl4.from(".detail4_h2", {y: 100, opacity: 0, duration: 1});
+
+let tl5 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".detail_pattern",
+        scrub: true,
+        start: "top bottom"
+    }
+});
+
+tl5.from(".detail_pattern", {x: 250})
